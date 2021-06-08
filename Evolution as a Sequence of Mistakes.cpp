6108f2a31b30f30f7fba459df17064331dfab6a9
Твоя Булочка <85406297+Romb1k@users.noreply.github.com>
2021-06-08 09:54:27 +0300

    Add files via upload

---
+++ Evolution as a Sequence of Mistakes.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1, s2;
+	cin >> s1 >> s2;
+	char sym1, sym2;
+	int count = 0;
+	for (int i = 0; i < s1.length(); i++){
+		sym1 = s1.at(i);
+		sym2 = s2.at(i);
+		if (sym1 != sym2) count++;
+	}
+	cout << count << endl;
+}
